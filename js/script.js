b223b76698de1440bb23d427e590d5b51817f61d
Andriy M Etcheverry <andriy.etcheverry@gmail.com>
2020-03-16 03:18:20 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 window.onload = () => {
     document.getElementById('button1').addEventListener('click', () => {
+        //play Dorime
+        audio = new Audio('../assets/dorime.mp3');
+        audio.currentTime = 2;
+        audio.play();
+
+        //remove buttons
         document.getElementById('main').remove();
         
+        //add circle
         var mainText = document.getElementById("circle");
         mainText.classList.add('main');
         mainText.innerText = `
@@ -17,11 +24,16 @@ window.onload = () => {
         ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⣿⣿⣦⠀⠹⣿⣷⣶⣦⣼⣿⣿⣿⣿
         ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣼⣿⣿⣿⣷⣄⣸⣿⣿⣿⣿⣿⣿⣿⣿
         ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿`;
-        audio = new Audio('../assets/dorime.mp3');
-        audio.currentTime = 2;
-        audio.play();
 
-        
+        //add back button
+        var mainChild = document.createElement('div');
+        mainChild.setAttribute('id', 'back');
+        mainChild.textContent = 'Back';
+        document.getElementById('circle').appendChild(mainChild);        
+        document.getElementById('back').addEventListener('click', () => {
+            console.log('works!');
+            location.reload();
+        })
     });
     document.getElementById('button2').addEventListener('click', () => {
         alert('You are invited to select another button');
